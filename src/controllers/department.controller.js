71e2b0e659d4b5a675c04782ea7ec633542c85a5
edabeyza <edabeyzaozeren@gmail.com>
2024-07-22 21:44:51 +0300

    up

--- src/controllers/department.controller.js
+++ src/controllers/department.controller.js
@@ -2,55 +2,61 @@
 /* -------------------------------------------------------
     EXPRESS - Personnel API
 ------------------------------------------------------- */
-
-const Department =  require('../models/department.model')
+const Department = require("../models/department.model");
 
 module.exports.department = {
-    list: async (req, res) => {
-        
-        const data = await res.getModelList(Department) // Pagination
-        // const data = await Department.find({})
+  list: async (req, res) => {
+    const data = await res.getModelList(Department);
 
-        res.status(200).send({
-            error: false,
-            data,
-            detail: await res.getModelListDetails(Department)
-        })
-    },
+    res.status(200).send({
+      error: false,
+      data,
+      detail: await res.getModelListDetails(Department),
+    });
+  },
 
-    create: async (req, res) => {
+  create: async (req, res) => {
+    const data = await Department.create(req.body);
+    res.status(201).send({
+      error: false,
+      data,
+    });
+  },
 
-        const data = await Department.create(req.body)
+  read: async (req, res) => {
+    const data = await Department.findOne({ _id: req.params.id });
 
-        res.status(201).send({
-            error: false,
-        })
-    },
+    res.status(200).send({
+      error: false,
+      data,
+    });
+  },
+  
+  //filter, update,options
+  // const options = { upsert: true };
+  // acknowledged: İşlemin MongoDB tarafından tanındığını gösterir.
+  // matchedCount: Filtre kriterleriyle eşleşen belge sayısını belirtir.
+  // modifiedCount: Güncellenen belge sayısını belirtir.
+  // upsertedId: Eğer upsert kullanıldıysa ve yeni bir belge oluşturulduysa, bu belgenin _id değeri burada yer alır.
+  // upsertedCount: upsert işlemi ile kaç belgenin oluşturulduğunu belirtir.
 
-    read: async (req, res) => {
+  update: async (req, res) => {
+    const data = await Department.updateOne({ _id: req.params.id }, req.body, {
+      runValidators: true,
+    });
 
-        const data = await Department.findOne({_id:req.params.id})
-        res.status().send({
-            error: false,
-        })
-    },
+    res.status(202).send({
+      error: false,
+      data,
+      new: await Department.findOne({ _id: req.params.id }),
+    });
+  },
 
-    update: async (req, res) => {
-
-        const data = await Department.updateOne({ _id:req.params.id }, req.body, { runValidators:true })
-        res.status().send({
-            error: false,
-            new: await Department.fndOne({_id:req.params.id})
-        })
-    },
-
-    delete: async (req, res) => {
-
-        await Department.deleteOne({_id: req.params.id})
-
-        res.status(data.deletedCount ? 204 : 404).send({
-            error: !data.deletedCount,
-            data
-        })
-    }
-}
+  delete: async (req, res) => {
+    const data = await Department.deleteOne({ _id: req.params.id });
+    res.status(data.deletedCount ? 204 : 404).send({
+      error: !data.deletedCount,
+      data,
+    });
+  },
+};
